79d9a19754cd659e8fc269816480afe6d7f1d9c0
Viveros Bedoya Yazuline <Yazuline.Viveros@carvajal.com>
2023-11-24 21:20:04 -0500

    Seagrega funcionalidad para cambiar estado, eliminar agregar todo

--- src/App.tsx
+++ src/App.tsx
@@ -30,18 +30,37 @@ const initialTodos =[
     complate: true,
   },
   
-  
-
-]
+  ]
 
 const App = () => {
-  const [todos, seTodos]= useState<Todo[]>(initialTodos);
+  const [todos, setTodos]= useState<Todo[]>(initialTodos);
   const addTodo=(text: string) =>{
     const newTodo={
       text,
       complate: false,
     };
-    seTodos([...todos, newTodo]);
+    setTodos([...todos, newTodo]);
+  }
+
+  const toogleTodo=(selectedTodo:Todo) => 
+    {
+       setTodos(prevTodo=>{
+        return prevTodo.map(todo=>{
+          if(todo.text===selectedTodo.text){
+            return {
+            ...todo,
+              complate:!todo.complate,
+            }
+          }
+          return todo;
+        })
+       })
+  }
+
+  const removeTodo=() => {
+    setTodos(prevTodo=>{
+      return prevTodo.filter(todo=>!todo.complate);
+    })
   }
   return (
     <div  className="container" >
@@ -53,9 +72,11 @@ const App = () => {
         <article key={todo.text}>
           <label htmlFor="todo">
             <input type="checkbox"  
-              checked={todo.complate} id='todo'/>
+              checked={todo.complate} id='todo'
+              onChange={()=>toogleTodo(todo)}/>
               {todo.text}
           </label>
+          <button type='button' className='contrast' onClick={()=>removeTodo(todo)}>remover</button>
         </article>
 
       )
